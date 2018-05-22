feb637b86a8cae2e96320b4fc863b7ec6c42f3e6
StephenSimko1 <39318654+StephenSimko1@users.noreply.github.com>
2018-05-22 23:43:27 +0000

    Update settings.py

--- stable_project/settings.py
+++ stable_project/settings.py
@@ -65,7 +65,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [BASE_DIR + '/static/templates'],
-        'APP_DIRS': True,
+        'APP_DIRS': False,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
